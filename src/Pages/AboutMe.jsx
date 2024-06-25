a4184370e181e5951227fe4329f7cf9aaa018efb
Sanan Ali <sananali340@gmail.com>
2024-06-25 15:19:45 +0500

    update

--- src/Pages/AboutMe.jsx
+++ src/Pages/AboutMe.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Helmet } from 'react-helmet';
 import aboutFavicon from "../images/ab.png"
-import { faUser, faBirthdayCake, faFlag, faEnvelope, faMapMarkerAlt } from "@fortawesome/free-solid-svg-icons";
+import { faMapMarkerAlt,faBuilding , faLaptop, faUniversity, faGraduationCap} from '@fortawesome/free-solid-svg-icons';
 
 
 
@@ -70,7 +70,7 @@ export default function Me() {
         <section id="AboutMe" className="about--section">
           <div className=" about--section--box">
             <div className="about--section--content">
-              
+
               <p className="about--section--description">
                 I'm a Web Designer and Front-end Developer with a mission to create clean and user-friendly experiences. I am passionate about crafting excellent software that not only enhances the digital world but also improves the lives of those it touches.
               </p>
@@ -79,10 +79,66 @@ export default function Me() {
               </p>
             </div>
           </div>
- 
-        </section>
-      </section>
 
+        </section>
+
+<section id="Experience" className="about--section">
+          <div className="about--section--box">
+            <h2 className="about--section--title">Experience</h2>
+            <div className="about--section--content">
+              <div className="experience--item">
+                <h3 className="experience--title">React JS Developer</h3>
+                <p className="experience--details">
+                <FontAwesomeIcon icon={faBuilding} /> Lofty Logix <span style={{float:"inline-end"}}>
+                   <FontAwesomeIcon icon={faMapMarkerAlt} /> Johar Town, Lahore</span>
+                  <br /><br />
+                  June 2023 - March 2024
+                  <span style={{float:"inline-end"}}>Full-time</span>
+                </p>
+              </div>
+              <div className="experience--item">
+                <h3 className="experience--title">Front-End Developer</h3>
+                <p className="experience--details">
+                <FontAwesomeIcon icon={faBuilding} /> Tech Cloud 
+                <span style={{float:"inline-end"}}> <FontAwesomeIcon icon={faLaptop} /> Remote Job</span>
+                  <br /><br />
+                  July 2022 - October 2022
+                  <span style={{float:"inline-end"}}>Full-time</span>
+                </p>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section id="Education" className="about--section">
+          <div className="about--section--box">
+            <h2 className="about--section--title">Education</h2>
+            <div className="about--section--content">
+              <div className="education--item">
+                <h3 className="education--title">Bachelor's Degree - Computer Science</h3>
+                <p className="education--details">
+                <FontAwesomeIcon icon={faUniversity}/> PMAS Arid University<span style={{float:"inline-end"}}>
+                  <FontAwesomeIcon icon={faMapMarkerAlt} /> Garden Town, Lahore</span>
+                  <br /><br />
+                  Graduated: July 2024
+                  <span style={{float:"inline-end"}}>4 Years</span>
+                </p>
+              </div>
+              <div className="education--item">
+                <h3 className="education--title">FSc Pre-Engineering</h3>
+                <p className="education--details">
+                <FontAwesomeIcon icon={faGraduationCap}/> Govt.Shalimar College <span style={{float:"inline-end"}}>
+                 <FontAwesomeIcon icon={faMapMarkerAlt} /> Shalimar Town, Lahore</span>
+                  <br /><br />
+                  Completed: May 2020
+                  <span style={{float:"inline-end"}}>2 Years</span>
+                </p>
+              </div>
+            </div>
+          </div>
+        </section>
+
+      </section>
     </>
   );
 }
