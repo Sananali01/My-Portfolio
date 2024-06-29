5dd34f7301fcfc89a74cda093fe81b0eb3d2e76a
Sanan Ali <sananali340@gmail.com>
2024-06-29 19:37:49 +0500

    do it

--- src/Pages/MyPortfolio.jsx
+++ src/Pages/MyPortfolio.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 import { Helmet } from "react-helmet";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faLink } from "@fortawesome/free-solid-svg-icons";
+import { faLink, faDatabase } from "@fortawesome/free-solid-svg-icons";
+import {
+  faReact,
+  faNode,
+  faHtml5,
+  faCss3Alt,
+  faJs,
+  faBootstrap,
+  faGithub,
+  faPhp,
+} from "@fortawesome/free-brands-svg-icons";
 import Pr1 from "../images/Projects/Project1.jpg";
 import Pr2 from "../images/Projects/Project2.png";
 import Pr3 from "../images/Projects/Project3.png";
@@ -14,13 +24,26 @@ import Pr9 from "../images/Projects/Project9.png";
 import Pr10 from "../images/Projects/Project10.png";
 import prFavicon from "../images/project.png";
 
+const technologyIcons = {
+  "React JS": { icon: faReact, color: "#009ac0" },
+  "Node JS": { icon: faNode, color: "#68A063" },
+  "HTML": { icon: faHtml5, color: "#E44D26" },
+  "CSS": { icon: faCss3Alt, color: "#264DE4" },
+  "JavaScript": { icon: faJs, color: "#EFD81D" },
+  "Bootstrap": { icon: faBootstrap, color: "#40236b" },
+  "Database": { icon: faDatabase, color: "#4DB33D" },
+  "Github": { icon: faGithub, color: "#000000" },
+  "Php": { icon: faPhp, color: "#777BB4" },
+  "Firebase": { icon: faDatabase, color: "#FFCA28" }
+};
+
 export default function MyPortfolio() {
   const portfolioData = [
     {
       title: "Campus Management System",
       description:
         "A comprehensive system for managing various aspects of campus life, including student information, course registration, and administrative tasks.",
-      technologies: ["React JS", "Node JS", "MongoDB","Material UI"],
+      technologies: ["React JS", "Node JS", "Database","Github"],
       src: Pr1,
       link: "https://github.com/Sananali01/Campus-Management-System",
     },
@@ -28,7 +51,7 @@ export default function MyPortfolio() {
       title: "Lofty Logix",
       description:
         "Lofty Logix is a state-of-the-art logistics management system designed to streamline operations and enhance efficiency.",
-      technologies: ["HTML", "CSS", "Bootstrap", "Javascript"],
+      technologies: ["HTML", "CSS", "Bootstrap", "JavaScript"],
       src: Pr2,
       link: "https://lofty-logix.vercel.app/",
     },
@@ -52,7 +75,7 @@ export default function MyPortfolio() {
       title: "Lofty Logix Portfolio",
       description:
         "A detailed portfolio showcasing the various projects and achievements of Lofty Logix.",
-      technologies: ["React Js"],
+      technologies: ["React JS"],
       src: Pr5,
       link: "https://loftylogix.com/",
     },
@@ -76,7 +99,7 @@ export default function MyPortfolio() {
       title: "Clatter Cloud",
       description:
         "Clatter Cloud is a cloud-based communication tool designed for teams to collaborate efficiently.",
-      technologies: ["HTML", "CSS", "JavaScript"],
+      technologies: ["HTML", "CSS"],
       src: Pr8,
       link: "https://clatter-cloud.netlify.app/",
     },
@@ -84,7 +107,7 @@ export default function MyPortfolio() {
       title: "Ticket Selling",
       description:
         "A streamlined platform for buying and selling event tickets with ease.",
-      technologies: ["React JS"],
+      technologies: ["HTML", "CSS"],
       src: Pr9,
       link: "https://ticket-landing-page.netlify.app/",
     },
@@ -92,52 +115,59 @@ export default function MyPortfolio() {
       title: "Coin Sensei",
       description:
         "CoinSensei.co is a cutting-edge cryptocurrency educational platform, offering expert insights and tutorials to empower individuals in the dynamic world of digital assets.",
-      technologies: ["React JS"],
+      technologies: ["React JS","Php"],
       src: Pr10,
       link: "https://www.coinsensei.co/",
     },
-  
   ];
 
   return (
     <>
-    <Helmet>
-      <title>Projects | Your Name</title>
-      <link rel="icon" type="image/png" href={prFavicon} />
-    </Helmet>
-    <section className="portfolio-section" id="MyPortfolio">
-      <div className="portfolio-container">
-        <h1 className="section-heading">My Projects</h1>
-        <p className="section-subheading">
-          Explore some of my projects that showcase my skills and expertise.
-        </p>
-      </div>
-      <div className="portfolio-grid">
-        {portfolioData.map((item, index) => (
-          <div key={index} className="portfolio-card">
-            <div className="portfolio-img">
-              <img src={item.src} alt={item.title} />
-            </div>
-            <div className="portfolio-card-content">
-              <h3 className="portfolio-title">{item.title}</h3>
-              <p className="portfolio-description">{item.description}</p>
-              <div className="portfolio-technologies">
-                <strong>Technologies:</strong> {item.technologies.join(", ")}
+      <Helmet>
+        <title>Projects | Your Name</title>
+        <link rel="icon" type="image/png" href={prFavicon} />
+      </Helmet>
+      <section className="portfolio-section" id="MyPortfolio">
+        <div className="portfolio-container">
+          <h1 className="section-heading">My Projects</h1>
+          <p className="section-subheading">
+            Explore some of my projects that showcase my skills and expertise.
+          </p>
+        </div>
+        <div className="portfolio-grid">
+          {portfolioData.map((item, index) => (
+            <div key={index} className="portfolio-card">
+              <div className="portfolio-img">
+                <img src={item.src} alt={item.title} />
               </div>
-              <a
-                href={item.link}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="portfolio-link"
-              >
-                <FontAwesomeIcon icon={faLink} /> Visit Project
-              </a>
+              <div className="portfolio-card-content">
+                <h3 className="portfolio-title">{item.title}</h3>
+                <p className="portfolio-description">{item.description}</p>
+                <div className="portfolio-technologies">
+                  <strong>Technologies:</strong>
+                  {item.technologies.map((tech, idx) => (
+                    <FontAwesomeIcon
+                      key={idx}
+                      icon={technologyIcons[tech].icon}
+                      title={tech}
+                      style={{ color: technologyIcons[tech].color, fontSize: "xx-large", margin: "0 8px" }}
+                      className="technology-icon"
+                    />
+                  ))}
+                </div>
+                <a
+                  href={item.link}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="portfolio-link"
+                >
+                  <FontAwesomeIcon icon={faLink} /> Visit Project
+                </a>
+              </div>
             </div>
-          </div>
-        ))}
-      </div>
-    </section>
-  </>
-
+          ))}
+        </div>
+      </section>
+    </>
   );
 }
